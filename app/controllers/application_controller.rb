677f724bf64ab6bb5353d91ed335fb97f6755572
Deepak Vig <deepakvig@gmail.com>
2018-08-19 20:01:10 +0530

    Shop method added back to Home Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def shop
-    p shop_session
-    return unless shop_session.present?
-    @shop ||= Shop.find_by(shopify_domain: shop_session.url)
-  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ShopifyApp::AuthenticatedController
     # Show all customers who have paid for their orders
     @customers = shop.customer_details
   end
+
+  private
+
+  def shop
+    p shop_session
+    @shop ||= Shop.find_by(shopify_domain: shop_session.url)
+  end
 end
